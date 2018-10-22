ad9b860f8e9f3de14661c3e88ddf8cf4c7abbf77
Sigrid Benezra <sdbenezra@gmail.com>
2018-10-21 21:32:24 -0700

    Finished array_intersection.

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,45 @@
 # Creates a new array to return the intersection of the two input arrays
+require 'pry'
+
 def intersection(array1, array2)
-  raise NotImplementedError
+  if array1 == nil || array2 == nil
+    return []
+  end
+
+  if array1 == [] || array2 == []
+    return []
+  end
+
+  if array1 == [] && array2 == []
+    return []
+  end
+
+  intersection = []
+  index1 = 0
+  index2 = 0
+  if array1.length >= array2.length
+    longer_array = array1
+    shorter_array = array2
+  else
+    longer array = array2
+    shorter_array = array1
+  end
+  while index1 < longer_array.length
+    while longer_array[index1] != shorter_array[index2]
+      if shorter_array[index2] == nil
+        index1 += 1
+        index2 = 0
+      else
+        index2 += 1
+      end
+    end
+    if longer_array[index1] != nil# binding.pry
+      intersection << longer_array[index1]
+      index1 += 1
+      index2 = 0
+    end
+    # binding.pry
+  end
+  return intersection
+  # raise NotImplementedError
 end
--- specs/array_intersection_spec.rb
+++ specs/array_intersection_spec.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require_relative '../lib/array_intersection'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe "array intersection" do
   describe "basic tests" do
